78df13844102b0392be8fe8e21300aa87b5c0eb0
Wilk <ma.wilk@tlen.pl>
2014-01-19 12:01:00 +0100

    inputinput

--- dodatkowy_input.sql
+++ dodatkowy_input.sql
@@ -26,7 +26,7 @@ GO
 --INSERT INTO ProgiCenowe (ID_Konferencji,ID_Progu) VALUES (5,2)
 --INSERT INTO ProgiCenowe (ID_Konferencji,ID_Progu) VALUES (5,3)
 
---SELECT * FROM Konferencja WHERE YEAR(DataRozpoczecia) LIKE '2014'
+----SELECT * FROM Konferencja WHERE YEAR(DataRozpoczecia) LIKE '2014'
 
 --EXEC dodaj_dzien_konferencji 1, '05/05/2014', 50
 --EXEC dodaj_dzien_konferencji 2, '05/11/2014', 100
@@ -35,7 +35,7 @@ GO
 --EXEC dodaj_dzien_konferencji 4, '03/13/2014', 120
 --EXEC dodaj_dzien_konferencji 5, '09/09/2014',1
 
---SELECT * FROM DzienKonferencji WHERE YEAR(DzienKonferencji) LIKE '2014'
+----SELECT * FROM DzienKonferencji WHERE YEAR(DzienKonferencji) LIKE '2014'
 
 --EXEC dodaj_warsztat 1, 6, 11, 2, '11:30:00', '14:30:00'
 --EXEC dodaj_warsztat 1, 6, 40, 1, '12:00:00', '15:00:00'
@@ -45,20 +45,25 @@ GO
 --EXEC dodaj_warsztat 1, 3, 100, 20, '12:35:00', '15:05:00'
 --EXEC dodaj_warsztat 1, 4, 110, 20, '12:00:00', '15:00:00'
 
---SELECT * FROM Warsztat
---SELECT * FROM Osoba
---SELECT * FROM Klient
+----SELECT * FROM Warsztat
+----SELECT * FROM Osoba
+----SELECT * FROM Klient
 
 --EXEC dodaj_klienta_osoba "Julian", "Zak", NULL, "873938701", "jzak@jestemailem.pl", "Slupska 156", "Szczecin", "70-233", "Polska"
 --EXEC dodaj_zamowienie 1, 5, "07/12/2012", 2, 1, 0, 0, "07/30/2012", 2
 --EXEC dodaj_zamowienie_szcz 1,6,1
 
---SELECT * FROM ZamowienieSzczegolowe
+----SELECT * FROM ZamowienieSzczegolowe
 --SELECT * FROM Zamowienie
 --EXEC dodaj_zamowienie_warsztatu 1,3,1,0
 --EXEC dodaj_zamowienie_warsztatu 1,2,1,0
 --EXEC dodaj_zamowienie_warsztatu 1,1,1,0
 --SELECT * FROM  ZamowienieWarsztatu
+--DELETE FROM ZamowienieWarsztatu
+--WHERE ID_ZamowieniaWarsztatu=1
+
+--DELETE FROM ZamowienieSzczegolowe
+--WHERE ID_ZamSzczegolowego=1
 
 --EXEC dodaj_uczestnika_konferencji 1,1
 
@@ -68,9 +73,6 @@ GO
 --EXEC dodaj_uczestnika_warsztatu 1,3
 --EXEC dodaj_uczestnika_warsztatu 1,2
 --EXEC dodaj_uczestnika_warsztatu 1,1
-
---DELETE FROM ZamowienieSzczegolowe
---WHERE ID_ZamSzczegolowego=1
 
 --SELECT * FROM Przychod
 --SELECT * FROM NieprawidlowaKwota
